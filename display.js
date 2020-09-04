e9bf1bd0ca1efefb9e43e9c34e644832e059edcb
fhancic <fhancic@croz.net>
2020-09-04 13:49:33 +0200

    basic helper, removed duplicate roms

--- display.js
+++ display.js
@@ -5,3 +5,23 @@ function init() {
     let ctx = canvas.getContext('2d');
     ctx.fillRect(0, 0, 640, 320);
 }
+
+async function showHelp(name) {
+    const response = await fetch(`help/${name}.html`)
+    if (!response.ok) {
+        clearHelp();
+        return;
+    }
+    const responseHtml = await response.text();
+
+    $('#gameHelp').fadeOut(500, function () {
+        $(this).html(responseHtml).fadeIn(500);
+    });
+    $('#gameHelp').prop('hidden', false);
+}
+
+function clearHelp() {
+    $('#gameHelp').fadeOut(500, function () {
+        $('#gameHelp').prop('hidden', true);
+    });
+}
